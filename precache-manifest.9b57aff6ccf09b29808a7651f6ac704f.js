40d18584f3fc214fcb1056ab9251f607b17c1e2e
BryanDGuy <bryan@DESKTOP-35CV6MT.localdomain>
2020-10-11 16:12:41 -0500

    Updates

---
+++ precache-manifest.9b57aff6ccf09b29808a7651f6ac704f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a097c0a1531961d72956d5fcdb2725",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "a7492056f758aa65ea65",
+    "url": "/Portfolio-Website/static/css/main.c2348554.chunk.css"
+  },
+  {
+    "revision": "9cb24f5f235701d5d78f",
+    "url": "/Portfolio-Website/static/js/2.9d1c1ab6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-Website/static/js/2.9d1c1ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7492056f758aa65ea65",
+    "url": "/Portfolio-Website/static/js/main.d4fa3943.chunk.js"
+  },
+  {
+    "revision": "adf109593c35843d1c70",
+    "url": "/Portfolio-Website/static/js/runtime-main.01f8dbdd.js"
+  }
+]);
